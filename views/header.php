e6a253235d102e85b44495a605d796f394060fe7
Aristotelis Agoratsios <80395059+aristotelisagoratsios@users.noreply.github.com>
2022-05-04 23:32:24 +0300

    Update header.php
    initial commit

--- views/header.php
+++ views/header.php
@@ -9,6 +9,10 @@
           <!-- Bootstrap CSS -->
            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
            <link  rel="stylesheet" type="text/css" href="https://webdevbuilder.tech/12-twitter/styles.php">
+
+<style type="text/css">
+
+</style>
            
 </head>
   <body class="d-flex flex-column h-100">
